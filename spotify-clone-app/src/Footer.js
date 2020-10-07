cb064fde0d3d4cee408296f161ce433aa52b3f6c
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-07 23:22:02 +0530

    css started

---
+++ spotify-clone-app/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Footer.css";
+function Footer()  {
+    return ( 
+        <div className="footer">
+            <h1>I am footer</h1>
+        </div>
+     );
+}
+ 
+export default Footer
--- spotify-clone-app/src/Player.js
+++ spotify-clone-app/src/Player.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Body from './Body';
+import Footer from './Footer';
+import "./Player.css";
+import Sidebar from './Sidebar';
 
 function Player({ spotify }) {
     return (
-        <div>
-            <h1>
-                Welcome to Spotify
-            </h1>
+        <div className="player">
+            <div className="player_body">
+                <Sidebar />
+                <Body />
+                <Footer />
+            </div>
         </div>
     )
 }
